542b7814bab8053e16301ddb20be03fc8c79a836
Emerson Ford <emersontford@gmail.com>
2020-04-15 17:46:49 -0600

    Fix usage of Puppet heredocs

--- manifests/kubernetes/cluster_cleanup.pp
+++ manifests/kubernetes/cluster_cleanup.pp
@@ -9,20 +9,20 @@ class slate::kubernetes::cluster_cleanup {
     path        => ['/usr/bin', '/bin', '/sbin', '/usr/local/bin'],
     environment => ['HOME=/root', 'KUBECONFIG=/etc/kubernetes/admin.conf'],
     command     => @(EOF/L)
-      set -euo pipefail; \
-      kubeadm token list \
-      | tail -n +2 \
-      | awk -v date=$(date +%FT%T%:z --date="-7 days") \'{if($2 == "<invalid>" && $3 < date) print $1;}\' \
+      set -euo pipefail;
+      kubeadm token list
+      | tail -n +2
+      | awk -v date=$(date +%FT%T%:z --date="-7 days") '{if($2 == "<invalid>" && $3 < date) print $1;}'
       | xargs -n1 kubeadm token delete
       | - EOF
       ,
     onlyif      => @(EOF/L)
-      set -euo pipefail; \
-      test $( \
-      kubeadm token list \
-      | tail -n +2 \
-      | awk -v date=$(date +%FT%T%:z --date="-7 days") \'{if($2 == "<invalid>" && $3 < date) print $1;}\' \
-      | wc -l \
+      set -euo pipefail;
+      test $(
+      kubeadm token list
+      | tail -n +2
+      | awk -v date=$(date +%FT%T%:z --date="-7 days") '{if($2 == "<invalid>" && $3 < date) print $1;}'
+      | wc -l
       ) -ge 1
       | - EOF
       ,
