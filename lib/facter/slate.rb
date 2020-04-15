b0e650de8f0a22bd258c05858855a2b806e43162
Emerson Ford <emersontford@gmail.com>
2020-04-15 17:41:32 -0600

    Fix lint errors and improve error handling

--- lib/facter/slate.rb
+++ lib/facter/slate.rb
@@ -9,16 +9,18 @@ Facter.add(:slate) do
   kubeadm = 'kubeadm'
   res = {}
 
+  Facter::Core::Execution.execute('yum list installed | grep kubelet')
+  return unless $?.exitstatus.zero?
+
+  Facter::Core::Execution.execute('yum list installed | grep kubeadm')
+  return unless $?.exitstatus.zero?
+
   kubelet_ver = Facter::Core::Execution.execute('kubelet --version')
-  if $?.exitstatus != 0
-    return
-  else
-    kubeadm_ver = Facter::Core::Execution.execute("kubeadm version -o short")
-    res["kubernetes"] = {
-      "kubelet_version" => (kubelet_ver.match(/Kubernetes v([0-9.]+)/))[1],
-      "kubeadm_version" => (kubeadm_ver.match(/v([0-9.]+)/))[1]
-    }
-  end
+  kubeadm_ver = Facter::Core::Execution.execute('kubeadm version -o short')
+  res['kubernetes'] = {
+    'kubelet_version' => kubelet_ver.match(/Kubernetes v([0-9.]+)/)[1],
+    'kubeadm_version' => kubeadm_ver.match(/v([0-9.]+)/)[1],
+  }
 
   Facter::Core::Execution.execute("#{kubectl} get nodes")
   if $?.exitstatus != 0
@@ -28,9 +30,9 @@ Facter.add(:slate) do
     return
   end
 
-  hostname = Facter.value(:networking)["fqdn"]
+  hostname = Facter.value(:networking)['fqdn']
   leader_info_json = Facter::Core::Execution.execute("#{kubectl} get ep/kube-scheduler -n kube-system -o json")
-  leader_info = JSON.parse(JSON.parse(leader_info_json)["metadata"]["annotations"]["control-plane.alpha.kubernetes.io/leader"])
+  leader_info = JSON.parse(JSON.parse(leader_info_json)['metadata']['annotations']['control-plane.alpha.kubernetes.io/leader'])
 
   # We want to prevent multiple hosts from creating new certificate keys, creating
   # race conditions when bringing up new control nodes. This ensures only the current
@@ -42,14 +44,14 @@ Facter.add(:slate) do
   # of a given certificate key is.
   #
   # Removes the tag beginning with _ from the holderIdentity string to get the hostname.
-  if hostname != leader_info["holderIdentity"].scan(/(.+)(_[a-z0-9-]+)/)[0][0]
-    res["kubernetes"]["leader"] = false
+  if hostname != leader_info['holderIdentity'].scan(/(.+)(_[a-z0-9-]+)/)[0][0]
+    res['kubernetes']['leader'] = false
     setcode do
       res
     end
     return
   else
-    res["kubernetes"]["leader"] = true
+    res['kubernetes']['leader'] = true
   end
 
   certificate_key = Facter::Core::Execution.execute("#{kubeadm} init phase upload-certs --upload-certs 2>/dev/null | egrep '^\\w{64}$'")
@@ -59,11 +61,11 @@ Facter.add(:slate) do
 
   discovery_tokens = Facter::Core::Execution.execute("#{kubeadm} token list | grep 'authentication,signing'")
 
-  discovery_token = ""
+  discovery_token = ''
 
   # Check if we already have a valid discovery token to pass out.
   discovery_tokens.each_line do |line|
-    token, ttl = line.split(" ")
+    token, ttl = line.split(' ')
     if ttl =~ /\d+h/
       discovery_token = token
       break
@@ -71,38 +73,39 @@ Facter.add(:slate) do
   end
 
   # If not, generate a new one.
-  if discovery_token == ""
+  if discovery_token == ''
     discovery_token = Facter::Core::Execution.execute("#{kubeadm} token create")
   end
-  res["kubernetes"] = res["kubernetes"].merge({
-    "certificate_key" => certificate_key,
-    "discovery_ca_cert_hash" => discovery_ca_cert_hash,
-    "discovery_token" => discovery_token,
-    "leader_acquire_time" => leader_info["acquireTime"],
-  })
+
+  res['kubernetes'] = res['kubernetes'].merge(
+    'certificate_key' => certificate_key,
+    'discovery_ca_cert_hash' => discovery_ca_cert_hash,
+    'discovery_token' => discovery_token,
+    'leader_acquire_time' => leader_info['acquireTime'],
+  )
 
   # Publish our control_plane_endpoint or apiserver_advertise_address.
   kubeadm_config_json = Facter::Core::Execution.execute("#{kubectl} get configmap/kubeadm-config -n kube-system -o json")
   kubeadm_config = JSON.parse(kubeadm_config_json)
 
-  cluster_status = YAML.load(kubeadm_config["data"]["ClusterStatus"])
-  cluster_config = YAML.load(kubeadm_config["data"]["ClusterConfiguration"])
+  cluster_status = YAML.safe_load(kubeadm_config['data']['ClusterStatus'])
+  cluster_config = YAML.safe_load(kubeadm_config['data']['ClusterConfiguration'])
 
   # Only present on a cluster set up as a high-availability cluster.
-  if cluster_config.key?("controlPlaneEndpoint")
-    cpe = cluster_config["controlPlaneEndpoint"].split(":")
-    res["kubernetes"] = res["kubernetes"].merge({
-      "control_plane_endpoint_hostname" => cpe[0],
-      "control_plane_endpoint_port" => cpe[1],
-    })
+  if cluster_config.key?('controlPlaneEndpoint')
+    cpe = cluster_config['controlPlaneEndpoint'].split(':')
+    res['kubernetes'] = res['kubernetes'].merge(
+      'control_plane_endpoint_hostname' => cpe[0],
+      'control_plane_endpoint_port' => cpe[1].to_i,
+    )
   # The cluster is a single availability cluster.
   else
-    cluster_status["apiEndpoints"].each_pair do |api_hostname, value|
-      res["kubernetes"] = res["kubernetes"].merge({
-        "apiserver_advertise_hostname" => api_hostname,
-        "apiserver_advertise_address" => value["advertiseAddress"],
-        "apiserver_advertise_port" => value["bindPort"],
-      })
+    cluster_status['apiEndpoints'].each_pair do |api_hostname, value|
+      res['kubernetes'] = res['kubernetes'].merge(
+        'apiserver_advertise_hostname' => api_hostname,
+        'apiserver_advertise_address' => value['advertiseAddress'],
+        'apiserver_advertise_port' => value['bindPort'].to_i,
+      )
       break
     end
   end
