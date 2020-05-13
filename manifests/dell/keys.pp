1af0b8ab6d80efae9eeb1fdb2a80daf9ddaefed9
Emerson Ford <emersontford@gmail.com>
2020-05-13 15:08:47 -0600

    Eliminate some Puppet redundancy and improve commenting

--- manifests/dell/keys.pp
+++ manifests/dell/keys.pp
@@ -13,21 +13,23 @@ class slate::dell::keys (
   }
 
   $key_names.each |String $key_name| {
-    exec { "download Dell key ${key_name}":
-      path      => '/bin:/usr/bin:/sbin:/usr/sbin',
-      command   => "curl -sSL https://linux.dell.com/repo/pgp_pubkeys/${key_name}.asc -o /usr/libexec/dell_dup/${key_name}.asc",
-      unless    => "test -f /usr/libexec/dell_dup/${key_name}.asc",
-      logoutput => 'on_failure',
-    }
     # Using https://linux.dell.com as a file source caused some strange self-signed certificate error
-    # with Puppet agent. The exec above is a workaround for this.
-    -> file { "/usr/libexec/dell_dup/${key_name}.asc":
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
+    # with Puppet agent. The exec is a workaround for this.
+    # I think this bug is due to https://tickets.puppetlabs.com/browse/PUP-7814.
+    exec { "download Dell key ${key_name}":
+      path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+      command => "curl -sSL https://linux.dell.com/repo/pgp_pubkeys/${key_name}.asc -o /usr/libexec/dell_dup/${key_name}.asc",
+      creates => "/usr/libexec/dell_dup/${key_name}.asc",
       require => File['/usr/libexec/dell_dup'],
     }
+
+    -> file { "/usr/libexec/dell_dup/${key_name}.asc":
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+    }
+
     -> exec { "import gpg key ${key_name} into RPM":
       path      => '/bin:/usr/bin:/sbin:/usr/sbin',
       command   => "rpm --import /usr/libexec/dell_dup/${key_name}.asc",
