473345b61d147d2821fc2c010972410360357ba6
Emerson Ford <emersontford@gmail.com>
2020-05-13 15:50:56 -0600

    Add comment about how to use this script

--- scripts/bootstrap_puppet.sh
+++ scripts/bootstrap_puppet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script will register a node with the CHPC Foreman/Puppet infrastructure.
+# If the node's domain is not whitelisted by the Puppet Master, a
+# `sudo /opt/puppetlabs/bin/puppetserver ca sign ...`
+# is required to be run on the Puppet master.
 
 rpm -Uvh https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
 
