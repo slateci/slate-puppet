3d72bfd782985cf662aefc513f2dc55e585db131
Mitchell <35324660+Mansalu@users.noreply.github.com>
2020-10-07 08:54:36 -0600

    add step to clean up iptables

--- scripts/nuke_k8s.sh
+++ scripts/nuke_k8s.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 kubeadm reset -f
 systemctl stop kubelet
 
@@ -12,3 +13,4 @@ fi
 if [ -d '/var/lib/etcd' ]; then
     rm -rf /var/lib/etcd
 fi
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
